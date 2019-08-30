675c9bbc2d6c8319b657bf6f382aef7fd3519a03
Alexis Segovia <53928507+424al@users.noreply.github.com>
2019-08-30 09:28:18 -0700

    Update depop.py

--- depop.py
+++ depop.py
@@ -5,10 +5,10 @@ from selenium.webdriver.common.keys import Keys
 
 
 ##USER INFORMATION
-user = ('greenpie')
-userpass = ('Rhs168909')
+user = ('YOUR USERNAME')
+userpass = ('YOUR PASSWORD')
 ## URL TO ACCOUNT YOU WANT TO FOLLOW ITS FOLLOWERS
-desired = ('https://www.depop.com/jrenner26/')
+desired = ('https://www.depop.com/ENTER USERS URL/')
 
 driver = webdriver.Chrome('/usr/local/bin/chromedriver')  # Optional argument, if not specified will search path.
 driver.get('https://www.depop.com/login/?redirect=/');
@@ -24,18 +24,23 @@ password.submit()
 time.sleep(5)
 
 
-#elem = driver.find_element_by_id('Logout')
-#if elem.is_displayed():
-	#	print('login successful')
+elem = driver.find_element_by_id('Logout')
+if elem.is_displayed():
+	print('login successful')
+else:
+	print('login error')
 # GOES TO DESIRED ACCOUNT'S PROFILE
 driver.get(desired)
 time.sleep(3)
 
 # OPENS UP FOLLOWER LIST
 driver.find_element_by_css_selector('[data-css-1k6tg18]').click()
-time.sleep(5)
+time.sleep(8)
 
+#scrolls down following list to load up followers
 scrolls = driver.find_elements_by_css_selector('[data-css-63oe3q]').send_keys(Keys.END)
+
+#methods to hit the follow button from popup
 
 follow = driver.find_elements_by_css_selector('[class="sc-lhVmIH bTiqlv"]')
 
@@ -43,6 +48,8 @@ followtwo = driver.find_elements_by_css_selector('[class="sc-ksYbfQ lmeZvI"]')
 
 followthree = driver.find_elements_by_name('follow')
 
+#follows users
+print('follwing users')
 for element in range(len(follow,)):
 	follow[element].click()
 	follow = driver.find_elements_by_css_selector('[class="sc-lhVmIH bTiqlv"]')
@@ -54,8 +61,9 @@ for element in range(len(followtwo,)):
 for element in range(len(followthree,)):
 	followthree[element].click()
 	followthree = driver.find_elements_by_name('follow')
-
 	
+#done following
+print('Following is done')
 
 
 
